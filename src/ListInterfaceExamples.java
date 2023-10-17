368760230e9077f7c4e4c341aa2289e782b554eb
Sar-taj107 <hussainarman065@gmail.com>
2023-10-18 01:05:52 +0530

    Arraylist Example

---
+++ src/ListInterfaceExamples.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+//or 
+import java.util.*;
+public class ListInterfaceExamples {
+
+    static void ArrayListExamples(){
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        System.out.println(l);                  //1,2,3
+        System.out.println(l.get(1));     //1-based indexing = 2
+
+        l.set(1,10 );             // modify at index 1
+        System.out.println(l);                  // 1, 10, 3
+  
+        System.out.println(l.contains(10));   //true
+
+        
+    }
+    public static void main(String[] args) {
+        ArrayListExamples();
+    }
+}
+
+/*
+ OUTPUT:
+ 
+[1, 2, 3]
+2
+[1, 10, 3]
+true
+
+ */
